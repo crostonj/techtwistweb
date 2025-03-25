fb29274c54951a63378227274d8e4d8311381dba
Jeff Croston <jcroston@3cloudsolutions.com>
2025-03-25 05:07:43 +0000

    Add styling for app layout and update App component structure

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
 import ProfilePage from "./components/ProfilePage";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <div>
+      <div className="app-container">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
+        <div className="content-container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
